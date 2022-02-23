4617774d415f06ea199ae306445ccf9b03feadba
Anıl Pak <71243066+anLpk@users.noreply.github.com>
2022-02-23 13:34:58 +1100

    sinif kavrami uzerine ilk alistirma tamamlandi.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace SinifKavrami
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          //1. Public : Her yerden erişilebilir.
+          //2. Private : Sadece tanımlandığı sınıf içerisinden erişilebilir.
+          // 3. Internal : Sadece bulunduğu proje içerisinden erişilebilir
+          //4. Protected : Sadece tanımlandığı sınıfta ya da o sınıfı miras alan sınıflardan erişilebilir.
+
+          Calisan calisan1 = new Calisan();
+          calisan1.Ad = "Anil";
+          calisan1.Soyad = "Pak";
+          calisan1.No = 12345678;
+          calisan1.Department = "IT";
+
+          calisan1.CalisanBilgileri();
+        }
+    }
+
+    class Calisan
+    {
+        public string Ad;
+        public string Soyad;
+        public int No;
+        public string Department;
+
+        public void CalisanBilgileri()
+        {
+            Console.WriteLine("Calisanin Adi:{0}", Ad);
+            Console.WriteLine("Calisanin Soyadi:{0}", Soyad);
+            Console.WriteLine("Calisanin Numarasi:{0}", No);
+            Console.WriteLine("Calisanin Departmani:{0}", Department);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-# SinifKavrami
+# C# Pratikler
+
+Bu repo [Kodluyoruz](https://www.kodluyoruz.org/) C# 101 egitimi kapsaminda olusturdugumuz alistirmalar reposu. Icersinde bir adet README dosyasi ve alistirmalar klasorleri mevcut.
+
+### Sinif Kavrami
+
+---
+
+## Installation
+
+---
+
+Öncelikle projeyi clonelayın.
+
+```
+git clone https://github.com/anLpk/SinifKavrami
+```
+
+## Usage
+
+---
+
+Projeyi cloneladıktan sonra Visual Studio Code programında açınız.
+Linux için:
+
+```
+cd SinifKavrami
+code .
+```
+
+## Contributing
+
+---
+
+Pull requestler kabul edilir. Büyük değişiklikler için, lütfen önce neyi değiştirmek istediğinizi tartışmak için bir konu açınız.
+
+## Licence
+
+---
+
+[MIT](https://choosealicense.com/licenses/mit/)
