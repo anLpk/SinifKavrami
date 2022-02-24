c546fc73eb16900604f3142d1cc51563f7ebc85c
Anıl Pak <71243066+anLpk@users.noreply.github.com>
2022-02-25 09:46:19 +1100

    enum alistirmasi tamamlandi

---
+++ Enum/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Enum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Gunler.Pazar);
+            Console.WriteLine((int)Gunler.Cumartesi); //numeric degeri verir.
+
+            int sicaklik = 32;
+
+            if(sicaklik <= (int)HavaSicakligi.Normal)
+            {
+                Console.WriteLine("Hava biraz soguk.");
+            }
+            //Diger Hava Sicakligi degerlerine gore else if ve else yazilabilir.
+        }
+    }
+
+    enum Gunler
+    {
+        Pazartesi,
+        Sali,
+        Carsamba,
+        Persembe,
+        Cuma,
+        Cumartesi,
+        Pazar
+    }
+
+    enum HavaSicakligi
+    {
+        Soguk = 5,
+        Normal = 20,
+        Sicak = 25,
+        CokSicak = 30
+    }
+}
