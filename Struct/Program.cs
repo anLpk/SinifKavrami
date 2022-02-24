c1604a7e6c32f4cc35544674850489796265ca99
Anıl Pak <71243066+anLpk@users.noreply.github.com>
2022-02-25 09:24:52 +1100

    struct ornekleri yapildi.

---
+++ Struct/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Struct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dikdortgen dikdortgen = new Dikdortgen();
+            dikdortgen.KisaKenar = 3;
+            dikdortgen.UzunKenar = 4;
+
+            Console.WriteLine("Class Alan Hesabi : {0}", dikdortgen.DikdortgenAlanHesabi());
+
+            Dikdortgen_Struct dikdortgen_Struct;
+            dikdortgen_Struct.KisaKenar = 3;
+            dikdortgen_Struct.UzunKenar = 4;
+
+            Console.WriteLine("Class Alan Hesabi : {0}", dikdortgen_Struct.DikdortgenAlanHesabi());
+        }
+    }
+
+    class Dikdortgen
+    {
+        public int KisaKenar;
+        public int UzunKenar;
+
+        public long DikdortgenAlanHesabi()
+        {
+            return this.KisaKenar * this.UzunKenar;
+        }
+    }
+
+    struct Dikdortgen_Struct
+    {
+        public int KisaKenar;
+        public int UzunKenar;
+
+        public long DikdortgenAlanHesabi()
+        {
+            return this.KisaKenar * this.UzunKenar;
+        }
+    }
+}
